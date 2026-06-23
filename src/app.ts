64097801c1787cb149206b7472006c153b4411a2
arafat717 <arafatjibon33@gmail.com>
2026-06-23 17:05:01 +0600

    added inital registration

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { prisma } from "./lib/prisma";
+import httpsStatus from "http-status-codes";
+import bycript from "bcrypt";
+
 const app = express();
 
 app.use(express.json());
@@ -19,6 +22,55 @@ app.get("/", async (req: Request, res: Response) => {
   const user = await prisma.user.findMany();
   console.log("users==>", user);
   res.send("Hello World!");
+});
+
+app.post("/api/v1/register", async (req: Request, res: Response) => {
+  const { email, name, password, profilePhoto } = req.body;
+
+  const isUserExits = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExits) {
+    throw new Error("User with the email already exists");
+  }
+
+  const hashPassword = await bycript.hash(password, 10);
+
+  const createUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  await prisma.profile.create({
+    data: {
+      userId: createUser.id,
+      profilePhoto,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createUser.id,
+      email: createUser.email || email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  res.status(httpsStatus.CREATED).json({
+    success: true,
+    statusCode: httpsStatus.CREATED,
+    message: "user registration successfully!",
+    data: { user },
+  });
 });
 
 export default app;
